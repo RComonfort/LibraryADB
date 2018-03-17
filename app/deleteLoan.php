7d7fdf77fb4c74307a3de65814fff52d123d25ab
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-03-17 16:36:59 -0600

    Correcion 8 de RUD de loans

--- app/deleteLoan.php
+++ app/deleteLoan.php
@@ -6,10 +6,11 @@
 	$db = new Database;
 	$loan = new Loan($db);
 	$loanID = filter_input(INPUT_GET, 'loan', FILTER_VALIDATE_INT);
-	echo $loanID;
-	die;
+	
 	if( $loanID ){
 		$loan->setLoanID($loanID);
+		echo $loanID;
+		die;
 		$loan->delete();
 	}
 	header("Location:" . Loan::baseurl() . "app/slpash.html");
