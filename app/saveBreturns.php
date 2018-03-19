84188508d818286bac9e8e56afda75c3df0a7f37
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-03-19 13:07:46 -0600

    Correccion 14 de C de breturns

--- app/saveBreturns.php
+++ app/saveBreturns.php
@@ -1,22 +1,17 @@
 <?php
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
-    require_once "../models/Breturn.php";
-	if (empty($_POST['submit'])){
-	      header("Location:" . Breturn::baseurl() . "app/listLoansToBreturns.php");
-	      exit;
-	}
-
-	$args = array(
-        'loan' => FILTER_SANITIZE_NUMBER_INT
-	);
-
-	$post = (object)filter_input_array(INPUT_POST, $args);
-
+	require_once "../models/Breturn.php";
+	
 	$db = new Database;
+	$loan = filter_input(INPUT_GET, 'loan', FILTER_VALIDATE_INT);
 	$breturn = new Breturn($db);
-	$breturn->setLoanID($post->loan);
-	$breturn->save();
+
+	if ( $loan ){
+		$breturn->setLoanID($loan);
+		$breturn->save();
+	}
+	
 	header("Location:" . Breturn::baseurl() . "app/listBreturns.php");
 
 ?>
